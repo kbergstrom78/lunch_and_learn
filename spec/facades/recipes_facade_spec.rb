d22e6836ccf1c4bd5c7500665dc0129253d2e9be
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-05 13:18:19 -0600

    test: add test file for recipes

---
+++ spec/facades/recipes_facade_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe RecipesFacade do
+  describe '.get_recipes' do
+    it 'returns recipes for a given country', :vcr do
+      country = 'Spain'
+      recipes = RecipesFacade.get_recipes(country)
+
+      expect(recipes).to be_an(Array)
+      expect(recipes.first).to be_a(Recipe)
+      expect(recipes.first.title).to be_a(String)
+    end
+
+    it 'handles an empty response gracefully', :vcr do
+      country = ''
+      recipes = RecipesFacade.get_recipes(country)
+
+      expect(recipes).to be_an(Array)
+      expect(recipes).to be_empty
+    end
+  end
+end
