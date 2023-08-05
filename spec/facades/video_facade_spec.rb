77e755c2c3a578e6461c4d2c36102920ebf373c2
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-05 14:17:19 -0600

    test: add test for video facade

---
+++ spec/facades/video_facade_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.describe VideoFacade, type: :model do
+  describe '.get_video' do
+    it 'returns a video for a given country', :vcr do
+      country = "USA"
+      video = VideoFacade.get_video(country)
+
+      expect(video).to be_a(Video)
+      expect(video.title).to be_a(String)
+    end
+
+    it 'returns nil if no videos are found', :vcr do
+      country = "KimsCountryofGreatness"
+      video = VideoFacade.get_video(country)
+
+      expect(video).to eq(nil)
+    end
+  end
+end
