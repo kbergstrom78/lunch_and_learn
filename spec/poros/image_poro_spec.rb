c4a9fa167e6a61fdeb9bf30299de9a722d518fb1
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-05 14:53:38 -0600

    test: add test for image PORO

---
+++ spec/poros/image_poro_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe Image, type: :model do
+  describe '#initialize' do
+    it 'creates an image from given attributes' do
+      image_data = {
+        alt_description: "Beautiful Sunset",
+        urls: {
+          regular: "https://example.com/sunset.jpg"
+        }
+      }
+
+      image = Image.new(image_data)
+
+      expect(image.alt_tag).to eq("Beautiful Sunset")
+      expect(image.url).to eq("https://example.com/sunset.jpg")
+    end
+  end
+end
