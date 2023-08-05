8caa5193aefec1a98933bd63e2c2dd1a2ee5538b
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-05 10:18:54 -0600

    test: recipe service tests

---
+++ spec/services/recipes_service_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe CountryService do
+  it '.all_countries', :vcr do
+    countries = CountryService.all_countries
+
+    expect(countries).to be_an Array
+
+    countries.each do |country|
+
+      expect(country).to have_key :name
+      expect(country[:name]).to have_key :common
+    end
+  end
+end
