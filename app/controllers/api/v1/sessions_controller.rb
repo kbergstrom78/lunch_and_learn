51910a81ef92b477b365fbb365d34bf66ba61f6d
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-06 10:13:18 -0600

    feat: sessions#create

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,29 @@
+class Api::V1::SessionsController < ApplicationController
+  skip_before_action :authenticate_request, only: [:create]
+
+  def create
+    user = User.find_by(email: session_params[:email])
+
+    if user && user.authenticate(session_params[:password])
+      render json: {
+        data: {
+          type: "user",
+          id: user.id,
+          attributes: {
+            name: user.name,
+            email: user.email,
+            api_key: user.api_key
+          }
+        }
+      }
+    else
+      render json: { error: 'Invalid credentials' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def session_params
+    params.permit(:email, :password)
+  end
+end
