1db0097a903a449970f79d9d02af20e22ef47406
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-06 10:36:00 -0600

    fix: update code and tests throughout due to authentication requirements

--- spec/requests/api/v1/recipes/search_spec.rb
+++ spec/requests/api/v1/recipes/search_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'The Recipes API', type: :request do
   describe 'GET /api/v1/recipes/search' do
+    let(:user) { create(:user) }
     it 'can find recipes based on chosen country', :vcr do
       chosen_country = 'italy'
 
-      get "/api/v1/recipes?country=#{chosen_country}"
-
+      get "/api/v1/recipes?country=#{chosen_country}", headers: { 'Authorization' => "Token token=\"#{user.api_key}\"" }
       expect(response).to be_successful
 
       recipes = JSON.parse(response.body, symbolize_names: true)
@@ -35,35 +35,36 @@ describe 'The Recipes API', type: :request do
     end
 
     it 'can find recipes based on a random country', :vcr do
-      get '/api/v1/recipes'
+      VCR.use_cassette('The_Recipes_API/can_find_recipes_based_on_a_random_country', record: :new_episodes) do
+        get '/api/v1/recipes', headers: { 'Authorization' => "Token token=\"#{user.api_key}\"" }
 
-      expect(response).to be_successful
+        expect(response).to be_successful
 
-      recipes = JSON.parse(response.body, symbolize_names: true)
+        recipes = JSON.parse(response.body, symbolize_names: true)
 
-      expect(recipes).to be_a(Hash)
-      expect(recipes).to have_key(:data)
-      expect(recipes[:data]).to be_an(Array)
+        expect(recipes).to be_a(Hash)
+        expect(recipes).to have_key(:data)
+        expect(recipes[:data]).to be_an(Array)
 
-      expect(recipes[:data][0]).to have_key(:id)
-      expect(recipes[:data][0][:id]).to eq(nil)
+        expect(recipes[:data][0]).to have_key(:id)
+        expect(recipes[:data][0][:id]).to eq(nil)
 
-      expect(recipes[:data][0]).to have_key(:type)
-      expect(recipes[:data][0][:type]).to eq('recipe')
+        expect(recipes[:data][0]).to have_key(:type)
+        expect(recipes[:data][0][:type]).to eq('recipe')
 
-      expect(recipes[:data][0]).to have_key(:attributes)
-      expect(recipes[:data][0][:attributes]).to have_key(:title)
-      expect(recipes[:data][0][:attributes]).to have_key(:url)
-      expect(recipes[:data][0][:attributes]).to have_key(:country)
-      expect(recipes[:data][0][:attributes]).to have_key(:image)
-      expect(recipes[:data][0][:attributes].keys.size).to eq(4)
+        expect(recipes[:data][0]).to have_key(:attributes)
+        expect(recipes[:data][0][:attributes]).to have_key(:title)
+        expect(recipes[:data][0][:attributes]).to have_key(:url)
+        expect(recipes[:data][0][:attributes]).to have_key(:country)
+        expect(recipes[:data][0][:attributes]).to have_key(:image)
+        expect(recipes[:data][0][:attributes].keys.size).to eq(4)
+      end
     end
 
     it 'returns an empty array when no recipes match the chosen country', :vcr do
       country = 'vatican city'
 
-      get "/api/v1/recipes?country=#{country}"
-
+      get "/api/v1/recipes?country=#{country}", headers: { 'Authorization' => "Token token=\"#{user.api_key}\"" }
       expect(response).to be_successful
 
       no_recipes = JSON.parse(response.body, symbolize_names: true)
@@ -75,8 +76,7 @@ describe 'The Recipes API', type: :request do
     end
 
     it 'returns an empty array when an empty string is given as a parameter', :vcr do
-      get "/api/v1/recipes?country="
-
+      get "/api/v1/recipes?country=", headers: { 'Authorization' => "Token token=\"#{user.api_key}\"" }
       expect(response).to be_successful
 
       no_recipes = JSON.parse(response.body, symbolize_names: true)
