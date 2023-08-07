66aaf6c97a82de99c182302e4baa64f149672301
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-07 09:40:33 -0600

    test: add test for get_capital to service spec

--- spec/services/country_service_spec.rb
+++ spec/services/country_service_spec.rb
@@ -11,4 +11,15 @@ RSpec.describe CountryService do
       expect(country[:name]).to have_key :common
     end
   end
+
+  it 'gets capital city', :vcr do
+    country_name = "France"
+    capital_city = CountryService.get_capital_city(country_name)
+
+    expect(capital_city).to be_a(String)
+    expect(capital_city).to eq("Paris")
+  end
+
+
+
 end
