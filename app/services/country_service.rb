382d557149db65f3668d731fc5f125e97dbe16aa
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-07 09:40:14 -0600

    build: add get_capital to country service

--- app/services/country_service.rb
+++ app/services/country_service.rb
@@ -7,4 +7,9 @@ class CountryService
     response = conn.get("/v3.1/all")
     JSON.parse(response.body, symbolize_names: true)
   end
+
+  def self.get_capital_city(country_name)
+    response = conn.get("/v3/name/#{country_name}")
+    JSON.parse(response.body, symbolize_names: true).first[:capital].first
+  end
 end
