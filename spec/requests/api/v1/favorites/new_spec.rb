af6eba44a2d242d05fcd2c0b8cad2408d1c4aa88
Kim Bergstrom <kimbergstrom78@gmail.com>
2023-08-08 09:28:13 -0600

    test: favorites#delete passing test

--- spec/requests/api/v1/favorites/new_spec.rb
+++ spec/requests/api/v1/favorites/new_spec.rb
@@ -32,5 +32,22 @@ RSpec.describe "Favorites", type: :request do
       expect(response).to have_http_status(:bad_request)
       expect(JSON.parse(response.body)['error']).to eq('Invalid API key')
     end
+
+    it "returns an error with invalid params" do
+      invalid_attributes = {
+        api_key: user.api_key,
+        country: "Thailand",
+        recipe_link: "",
+        recipe_title: ""
+      }
+
+      post "/api/v1/favorites", params: invalid_attributes.to_json, headers: {
+        'Content-Type' => 'application/json',
+        'Accept' => 'application/json'
+      }
+
+      expect(response).to have_http_status(:unprocessable_entity)
+      expect(JSON.parse(response.body)['error']).not_to be_empty
+    end
   end
 end
